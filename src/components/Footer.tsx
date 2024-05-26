f7f4560818e0b05dc4b43e6c332afa01f764dec6
msigurd <101131709+msigurd@users.noreply.github.com>
2024-05-26 22:40:49 +0200

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,11 @@ import { GitHubLogoIcon } from "@radix-ui/react-icons"
 const Footer: React.FC = () => (
   <div className="text-center">
     <p>
-      Tilbakemeldinger eller feil? Ta kontakt på&nbsp;
-      <a href="mailto:markus@kortspillguiden.no" className="underline">
-        markus@kortspillguiden.no
-      </a>
-      &nbsp;eller opprett et issue på&nbsp;
+      Tilbakemeldinger eller feil? Opprett et issue på&nbsp;
       <a
         target="_blank"
         rel="noopener noreferrer"
-        href="https://github.com/msigurd/Kortspillguiden.no"
+        href="https://github.com/msigurd/Kortspillguiden/issues"
         className="inline-flex align-top gap-0.5 underline"
       >
         <GitHubLogoIcon className="mt-1" />
@@ -20,7 +16,7 @@ const Footer: React.FC = () => (
     </p>
     <br />
     <p>
-      &copy; 2023&nbsp;
+      &copy; 2023-2024&nbsp;
       <a
         target="_blank"
         rel="noopener noreferrer"
